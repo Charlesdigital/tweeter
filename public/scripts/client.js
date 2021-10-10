f194bc8146b1d77e1271a62c4ba06287ec8dfd0b
Charlesdigital <charles.easton123@gmail.com>
2021-10-09 20:44:05 -0400

    fetch tweets with Ajax and Form Validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,12 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//Prevents page from refreshing on submit, converts form data to a query string and sends a post request to the server
 $(document).ready(function () {
   $("#tweetForm").submit(function (event) {
     event.preventDefault();
     const formData = $("#tweetForm").serialize();
     console.log(formData);
-    $.post("http://localhost:8080/tweets", formData, () => {});
+    const textLength = $("#tweet-text").val().length;
+    console.log(textLength);
+    if (textLength === 0) {
+      alert("Character must be greater than 0");
+    } else if (textLength > 140) {
+      alert("Character limit is 140");
+    } else {
+      $.post("http:/tweets", formData, () => {});
+    }
   });
 
   function createTweetElement(tweet) {
@@ -38,31 +47,30 @@ $(document).ready(function () {
 
     return $tweet;
   }
-  const tweetData = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
+  // const tweetData = [
+  //   {
+  //     user: {
+  //       name: "Newton",
+  //       avatars: "https://i.imgur.com/73hZDYK.png",
+  //       handle: "@SirIsaac",
+  //     },
+  //     content: {
+  //       text: "If I have seen further it is by standing on the shoulders of giants",
+  //     },
+  //     created_at: 1461116232227,
+  //   },
+  //   {
+  //     user: {
+  //       name: "Descartes",
+  //       avatars: "https://i.imgur.com/nlhLi3I.png",
+  //       handle: "@rd",
+  //     },
+  //     content: {
+  //       text: "Je pense , donc je suis",
+  //     },
+  //     created_at: 1461113959088,
+  //   },
+  // ];
   const renderTweets = function (tweets) {
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
@@ -73,45 +81,23 @@ $(document).ready(function () {
     // takes return value and appends it to the tweets container
   };
 
-  renderTweets(tweetData);
+  function loadTweets() {
+    $.ajax("/tweets", { method: "GET", dataType: "json" }).then(function (
+      results
+    ) {
+      console.log("Success: ", results);
+      renderTweets(results);
+    });
+  }
+
+  loadTweets();
+
+  //Loop through the array of data objects and display a new tweet article
 });
 
 // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 //$(document).ready(function () { include it, wait for the html to load
 //prepend to add at the top
-
-/* <section id = "tweets-container">
-<article class ="tweet">
-<header>
-<div class = "headerLeft">
-<img src = ${tweetData.user.avatars}">
-<p>${tweetData.user.name}</p>
-</div>
-<p>${tweetData.user.handle}</p>
-</header>
-<div>
-<p>${tweetData.content.text}</p>
-</div>
-<footer class = "articleFooter">
-<p>${tweetData.content.created_at}</p>
-<div class = "icons">
-<i class="fas fa-flag"></i>
-<i class="fas fa-retweet"></i>
-<i class="fas fa-heart"></i>
-</div>
-</footer>
-</article>
-</section>`
-
-
-
-
-
-
-
-const $tweet = $(`
-
-`); */
 
 /* const $tweet = $(`<article class="tweet">${tweetData.user.name}</article>`); */
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,11 @@ $(document).ready(function () {
   // --- our code goes here ---
   const counter = $("#tweet-text").on("input", function () {
     // console.log(this.value); //The this keyword is a reference to the button
-    $(this).parent().parent().find(".counter").val(140 - this.value.length);
+    $(this)
+      .parent()
+      .parent()
+      .find(".counter")
+      .val(140 - this.value.length);
 
     if (this.value.length > 140) {
       $(".counter").css("color", "red");
@@ -10,8 +14,15 @@ $(document).ready(function () {
       $(".counter").css("color", "black");
     }
   });
-});
 
+  // const tweetCheck = $("#tweet-button").on("submit", function () {
+  //   const textLength = $("#tweet-text").val().length;
+  //   console.log(textLength);
+  // });
+  // if (this.value.length > 140) {
+  //   $(".counter").css("color", "red");
+  // }
+});
 //$("") document.querySelector - targets classes and ids - return a jquery dom node
 
 //jquery have helper functions
@@ -20,5 +31,3 @@ $(document).ready(function () {
 //<div class="textArea"> is the parent of the input, not the parent of the counter .parent()
 
 //using .parent().parent() gets you the   <section class="new-tweet">, which gets you access to the 140 since its in section
-
-
